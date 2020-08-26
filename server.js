3fc13fbc37c3cf9d79e6f4e904569e61c7168bae
Toby Jorris <tobyjorris@gmail.com>
2020-08-26 08:07:36 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const express = require('express')
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const listening = port => {
+    console.log('listening on port', port)
+}
+
+const server = app.listen(port, listening(port));
